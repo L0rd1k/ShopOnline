c4368f2015616d42e6dcf9f7736d85bc9bd4c5db
Ilya Petrikov <ilua_1108@mail.ru>
2019-05-01 21:58:02 +0300

    Class for checking internet connection

---
+++ OnlineStore/CheckInternet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+namespace OnlineStore
+{
+    class CheckInternet
+    {
+        [DllImport("wininet.dll", CharSet = CharSet.Auto)]
+        static extern bool InternetGetConnectedState(ref InternetConnectionState lpdwFlags, int dwReserved);
+        [Flags]
+
+        enum InternetConnectionState : int
+        {
+            INTERNET_CONNECTION_MODEM = 0x1,
+            INTERNET_CONNECTION_LAN = 0x2,
+            INTERNET_CONNECTION_PROXY = 0x4,
+            INTERNET_RAS_INSTALLED = 0x10,
+            INTERNET_CONNECTION_OFFLINE = 0x20,
+            INTERNET_CONNECTION_CONFIGURED = 0x40
+        }
+
+        public static bool CheckForInternetConnection_Wininet()
+        {
+            InternetConnectionState flags = 0;
+            if (InternetGetConnectedState(ref flags, 0) == true)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool CheckForInternetConnection_Ping()
+        {
+            try
+            {
+                Ping pingObj = new Ping();
+                PingOptions pingOption = new PingOptions();
+                byte[] buffer = new byte[32];
+                PingReply reply = pingObj.Send("8.8.8.8", 1000, buffer, pingOption); // ping google directly through IP
+                if (reply.Status == IPStatus.Success)
+                    return true;
+                else
+                    return false;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ OnlineStore/LoginForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineStore
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            InternetConnection();
+        }
+
+        private void InternetConnection()
+        {
+            bool checkNet1 = CheckInternet.CheckForInternetConnection_Wininet();
+            bool checkNet2 = CheckInternet.CheckForInternetConnection_Ping();
+            if (checkNet1 && checkNet2)
+                MessageBox.Show("Internet conncetion : ONLINE!");
+            else
+                MessageBox.Show("Internet conncetion : OFFLINE!");
+        }
+
+
+    }
+}
